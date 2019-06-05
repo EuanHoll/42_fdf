9c0f283af5a665f2d3ad8d44968ce3749cda6347
Euan Hollidge <ehollidg@f0r2s1.codam.nl>
2019-06-05 18:14:54 +0200

    float -> double

--- srcs/dlgradient.c
+++ srcs/dlgradient.c
@@ -6,27 +6,27 @@
 /*   By: ehollidg <ehollidg@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/06/05 17:56:44 by ehollidg       #+#    #+#                */
-/*   Updated: 2019/06/05 18:09:07 by ehollidg      ########   odam.nl         */
+/*   Updated: 2019/06/05 18:13:25 by ehollidg      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-static float	gradloop(int x0, int y0, int x1, int y1)
+static double	gradloop(int x0, int y0, int x1, int y1)
 {
 	int dx;
 	int dy;
-	float gradient;
+	double gradient;
 
 	dx = x1 - x0;
 	dy = y1 - y0;
-	gradient = (float)((float)dy / (float)dx);
+	gradient = (double)((double)dy / (double)dx);
 	if (dx == 0)
 		gradient = 1.0;
 	return (gradient);
 }
 
-float			dlgradient(int x0, int y0, int x1, int y1)
+double			dlgradient(int x0, int y0, int x1, int y1)
 {
 	if (abs(y1 - y0) > abs(x1 - x0))
 	{
@@ -38,5 +38,5 @@ float			dlgradient(int x0, int y0, int x1, int y1)
 		swap(&x0, &x1);
         swap(&y0, &y1);
 	}
-	return (dlgradient(x0, y0, x1, y1));
+	return (gradloop(x0, y0, x1, y1));
 }
